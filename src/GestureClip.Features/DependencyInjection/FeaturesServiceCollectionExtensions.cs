3ca2fb669e693b418e2ab564cda78192df030233
loen jinitaimei loenkk <hhuhuwang@gmail.com>
2026-07-06 09:39:16 +0800

    feat: add first-run onboarding (src/GestureClip.Features/DependencyInjection/FeaturesServiceCollectionExtensions.cs)

--- src/GestureClip.Features/DependencyInjection/FeaturesServiceCollectionExtensions.cs
+++ src/GestureClip.Features/DependencyInjection/FeaturesServiceCollectionExtensions.cs
@@ -1,12 +1,13 @@
 using GestureClip.Core.Abstractions;
 using GestureClip.Features.Clipboard;
-using GestureClip.Features.Gestures;
 using GestureClip.Features.Diagnostics;
+using GestureClip.Features.Gestures;
+using GestureClip.Features.Onboarding;
 using GestureClip.Features.Privacy;
 using GestureClip.Features.Runtime;
 using GestureClip.Features.Startup;
-using GestureClip.Features.Workstation;
 using GestureClip.Features.WorkerLevel;
+using GestureClip.Features.Workstation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace GestureClip.Features.DependencyInjection;
@@ -29,6 +30,7 @@ public static class FeaturesServiceCollectionExtensions
         services.AddSingleton<IMouseGestureService, MouseGestureService>();
         services.AddSingleton<IEdgeTriggerService, EdgeTriggerService>();
         services.AddSingleton<IFeatureToggleService, FeatureToggleService>();
+        services.AddSingleton<IFirstRunOnboardingService, FirstRunOnboardingService>();
         services.AddSingleton<IDiagnosticsService, DiagnosticsService>();
         services.AddSingleton<IWorkstationStatsRepository, WorkstationStatsRepository>();
         services.AddSingleton<IWorkstationDashboardService, WorkstationDashboardService>();
@@ -39,4 +41,3 @@ public static class FeaturesServiceCollectionExtensions
         return services;
     }
 }
-
